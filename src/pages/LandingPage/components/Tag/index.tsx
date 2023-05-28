e2af2dbb8ec1f8f14a08f8a14b46b6fe6f581eec
unun <pattarawadee.ttt@gmail.com>
2023-05-28 18:24:43 +0700

    feat: converts long numbers into pretty

--- src/pages/LandingPage/components/Tag/index.tsx
+++ src/pages/LandingPage/components/Tag/index.tsx
@@ -1,5 +1,6 @@
 import { GIFT_CONFIG } from '../../../../configs'
 import { Wrapper, Gift, Amount } from './styled'
+import millify from 'millify'
 
 interface TagProps {
   gift: keyof typeof GIFT_CONFIG
@@ -10,7 +11,7 @@ const Tag = ({ gift, amount }: TagProps) => {
   return (
     <Wrapper color={GIFT_CONFIG[gift].colors.tagBackground}>
       <Gift src={GIFT_CONFIG[gift].giftImg} alt={gift} />
-      <Amount>x {amount}</Amount>
+      <Amount>x {millify(amount, { precision: 2 })}</Amount>
     </Wrapper>
   )
 }
